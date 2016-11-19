bbda59bec5a7ec4a7b7efcc593d42ca8e11f0823
oofdon2540 <komsan_58@hotmail.com>
2016-11-19 22:33:36 +0000

    treatment page and JavaScript

---
+++ price.js
@@ -0,0 +1,50 @@
+function priceChecker(){
+						var people=parseInt(prompt("How many people?"));
+						var treatment=prompt("What treatment would you like?");
+							if(treatment=="Authen" || treatment=="Authen" ){
+								if(people==1){
+									alert();
+								}
+							}
+						
+	
+	
+			
+			
+			
+			
+			
+			
+			
+
+
+
+
+
+					}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
