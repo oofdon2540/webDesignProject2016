dbbb98e519eed08048ebb9e8898fb6dae795ad41
oofdon2540 <komsan_58@hotmail.com>
2016-11-21 20:22:40 +0000

    put image in folder

--- price.js
+++ price.js
@@ -1,27 +1,10 @@
 function priceChecker(){
-						var people=parseInt(prompt("How many people?"));
-						var treatment=prompt("What treatment would you like?");
-							if(treatment=="Authen" || treatment=="Authen" ){
-								if(people==1){
-									alert();
-								}
-							}
-						
-	
-	
-			
-			
-			
-			
-			
-			
-			
+var price=parseInt(prompt("How many people?");
+var  treatment=prompt("What treatment?");
+	if(treatment == "hotoil"){
+		alert("50")
+	}		
 
-
-
-
-
-					}
 	
 	
 	
@@ -43,8 +26,4 @@ function priceChecker(){
 	
 	
 	
-	
-	
-	
-	
-	
+}
